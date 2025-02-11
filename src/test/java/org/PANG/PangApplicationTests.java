00f8aa77f691f5e2d514536f2e4137a9d66984ec
Kyoungkwan <young.kwin@gmail.com>
2025-02-11 15:41:09 +0900

    git init

---
+++ src/test/java/org/PANG/PangApplicationTests.java
@@ -0,0 +1,13 @@
+package org.PANG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
