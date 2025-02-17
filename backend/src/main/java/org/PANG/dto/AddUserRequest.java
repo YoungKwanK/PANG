531b7e3266f1a4dea450aa21777c708764c43a78
Kyoungkwan <young.kwin@gmail.com>
2025-02-17 16:41:41 +0900

    [추가] OAuth2 로그인 백엔드 구현
    원인 :
    
    수정사항 :
    
    영향 범위 :

---
+++ backend/src/main/java/org/PANG/dto/AddUserRequest.java
@@ -0,0 +1,14 @@
+package org.PANG.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Setter
+@Getter
+public class AddUserRequest {
+    private String email;
+    private Date birth;
+    private String residence;
+}
---
+++ backend/src/main/java/org/PANG/dto/CreateAccessTokenResponse.java
@@ -0,0 +1,10 @@
+package org.PANG.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreateAccessTokenResponse {
+    private String accessToken;
+}
