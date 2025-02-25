3f47b803a2c3a97aa82384840d810c84456053ad
gyongcode <gyongcode@gmail.com>
2025-02-25 14:55:44 +0900

    [수정] 구글 로그인 버튼 수정
    원인 : Oauth 구조 변경
    
    수정사항 : 서버에서 Oauth 전부 처리
    
    영향 범위 :

--- frontend/src/components/GoogleLoginButton.js
+++ frontend/src/components/GoogleLoginButton.js
@@ -1,41 +1,14 @@
-import React from "react";
-import { useGoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-
-
-// 구글 로그인 버튼 컴포넌트
 const GoogleLoginButton = () => {
-    const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID; // 환경 변수 사용
 
-    const GoogleLoginButton = () => {
-        const login = useGoogleLogin({
-            onSuccess: async (response) => {
-                console.log("Google Login Success:", response);
-
-                try {
-                    const res = await axios.post("/api/auth/google", {
-                        code: response.code, // 인가 코드 전달
-                    });
-                    console.log("Backend Response:", res.data);
-                } catch (error) {
-                    console.error("Error sending code to backend", error);
-                }
-            },
-            flow: "auth-code", // 인가 코드 방식 사용
-        });
-
-        return <button onClick={() => login()}>Login with Google</button>;
+    const goLogin = () => {
+        window.location.href = "http://localhost:8080/oauth2/authorization/google"
     };
-    
+
     return (
-        <GoogleOAuthProvider clientId={clientId}>
-            <div>
-                <h1>Google OAuth2 Login</h1>
-                <GoogleLoginButton />
-            </div>
-        </GoogleOAuthProvider>
+        <div>
+        <button onClick={goLogin}>구글 로그인 버튼</button>
+        </div>
     );
-};
+}
 
 export default GoogleLoginButton;
