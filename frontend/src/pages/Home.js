7b763962ad193a4024592d09490dfea3d4d55d8a
“YoungKwanK” <young.kwin@gmail.com>
2025-03-12 09:08:10 +0900

    [추가]KakKaoMap API 사용
    원인 : 지도 시각화를 위한 API 사용
    
    수정사항 :
    
    영향 범위 :

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Map } from "react-kakao-maps-sdk";
 import '../styles/Home.css';
 
 function Home() {
     console.log("Home 컴포넌트 렌더링됨"); // 콘솔로 확인해보기
-
-
     return (
         <div className="home">
             <h1>Home</h1>
+            <Map
+                center={{ lat: 33.450701, lng: 126.570667 }}
+                style={{ width: '1000px', height: '600px' }}
+                level={3}
+            />
 
         </div>
     )
